e06c40d06dc071e5d6a33fb68ad20465e540d556
vijaykalyan228 <vijaykalyan228@gmail.com>
2017-07-24 14:34:23 +0530

    Moved placeholder img to config-gmaps-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ProductCardsComponent } from './product-cards/product-cards.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import {routing, appRoutingProviders } from './app.routing';
-
-import { MdToolbarModule, MdToolbarRow, MdMenuModule, MdButtonModule, MdSlideToggleModule,
-   MdIconModule, MdCardModule, MdTabsModule, MdSelectModule, MdSidenavModule } from '@angular/material';
-import "hammerjs";
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { LocationsComponent } from './ctl-ethernet/locations/locations.component';
 import { CtlEthernetComponent } from './ctl-ethernet/ctl-ethernet.component';
 import { ConfigComponent } from './ctl-ethernet/config/config.component';
 import { QuotePricingComponent } from './quote-pricing/quote-pricing.component';
 import { ConfigGmapsComponent } from './ctl-ethernet/config/config-gmaps/config-gmaps.component';
+
+import { MdToolbarModule, MdToolbarRow, MdMenuModule, MdButtonModule, MdSlideToggleModule,
+   MdIconModule, MdCardModule, MdTabsModule, MdSelectModule, MdSidenavModule } from '@angular/material';
+import "hammerjs";
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -35,6 +36,9 @@ import { ConfigGmapsComponent } from './ctl-ethernet/config/config-gmaps/config-
     FormsModule,
     HttpModule,
     routing,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDOaapNH4pl_2CeEJP2HUqhEWwtfWqD0Bs'
+    }),
     MdToolbarModule, MdMenuModule, MdButtonModule, MdSidenavModule,
      MdIconModule, MdCardModule, MdTabsModule, MdSelectModule, MdSlideToggleModule
   ],
@@ -42,3 +46,4 @@ import { ConfigGmapsComponent } from './ctl-ethernet/config/config-gmaps/config-
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// AIzaSyDOaapNH4pl_2CeEJP2HUqhEWwtfWqD0Bs
--- src/app/ctl-ethernet/config/config-gmaps/config-gmaps.component.ts
+++ src/app/ctl-ethernet/config/config-gmaps/config-gmaps.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigGmapsComponent implements OnInit {
 
+  lat: number = 9.9789914;
+  lng: number = 76.317617;
+
   constructor() { }
 
   ngOnInit() {
